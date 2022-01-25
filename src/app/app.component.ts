a3119cb230e1d5950d971ecea4a8108541059a2b
schubert <schubertdeabreu2@gmail.com>
2022-01-25 09:15:05 +0530

    main selection sticky, intro para added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,23 @@ export class AppComponent {
   title = 'dataviz-palette-tool';
 
   default_palette: string[] = ["#4F091D", "#DD4A48", "#f7f3e8", "#97BFB4"];
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e) {
+     if (window.scrollY > window.innerHeight * ( 30 / 100)) {
+
+      let main_selection = document.getElementById('main-selection');
+      main_selection.style.position="fixed";
+      main_selection.style.top = "0";
+
+      document.getElementById("main").style.marginTop="9%";
+
+     } else {
+      let main_selection = document.getElementById('main-selection');
+      main_selection.style.position="static";
+
+      document.getElementById("main").style.marginTop="0";
+     }
+  }
+  
 }
